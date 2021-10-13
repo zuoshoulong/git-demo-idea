06d4992895c35bebb6a9474c4d17d59dbebf677d
zsl <1318224244@qq.com>
2021-10-13 16:47:20 +0800

    master commit5

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("hello master!");
         System.out.println("hello hot-fix!");
+        System.out.println("hello hot-fixxxxxxxxxxxxxxx!");
     }
 }
